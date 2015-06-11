4164d2d66291f4e65222e1dda46ccfb6fe5f9ff7
dienofail <alvinhzshi@yahoo.com>
2015-06-10 20:41:44 -0400

    a

--- DienoRumble/Program.cs
+++ DienoRumble/Program.cs
@@ -40,9 +40,80 @@ namespace DienoRumble
             E = new Spell(SpellSlot.E, 950);
             R = new Spell(SpellSlot.R, 1750);
             IgniteSlot = Player.GetSpellSlot("SummonerDot");
-            Q.SetSkillshot(0.25f, 125f, float.MaxValue, false, SkillshotType.SkillshotLine);
-            E.SetSkillshot(0.25f, 2000f, 2500f, false, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.25f, 125f, float.MaxValue, false, SkillshotType.SkillshotCone);
+            E.SetSkillshot(0.25f, 70f, 2000f, true, SkillshotType.SkillshotLine);
             R.SetSkillshot(float.MaxValue, 55f, 2000f, false, SkillshotType.SkillshotLine);
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            TargetSelector.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+    
+            Config.AddSubMenu(new Menu("Combo", "Combo"));
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseQCombo", "Use Q").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "Use E").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseRCombo", "Use R").SetValue(true));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseIgniteCombo", "Use Ignite").SetValue(true));
+
+            //Harass menu:
+            Config.AddSubMenu(new Menu("Harass", "Harass"));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseQHarass", "Use Q").SetValue(true));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseEHarass", "Use E").SetValue(false));
+            Config.SubMenu("Harass")
+                .AddItem(
+                    new MenuItem("HarassActive", "Harass!").SetValue(
+                        new KeyBind(Config.Item("Farm").GetValue<KeyBind>().Key, KeyBindType.Press)));
+            Config.SubMenu("Harass")
+                .AddItem(
+                    new MenuItem("HarassActiveT", "Harass (toggle)!").SetValue(new KeyBind("Y".ToCharArray()[0],
+                        KeyBindType.Toggle)));
+
+            //Farming menu:
+            Config.AddSubMenu(new Menu("Farm", "Farm"));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseQFarm", "Use Q").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Both", "No" }, 2)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseWFarm", "Use W").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Both", "No" }, 1)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("UseEFarm", "Use E").SetValue(
+                        new StringList(new[] { "Freeze", "LaneClear", "Both", "No" }, 3)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("FreezeActive", "Freeze!").SetValue(
+                        new KeyBind(Config.Item("Farm").GetValue<KeyBind>().Key, KeyBindType.Press)));
+            Config.SubMenu("Farm")
+                .AddItem(
+                    new MenuItem("LaneClearActive", "LaneClear!").SetValue(
+                        new KeyBind(Config.Item("LaneClear").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            //JungleFarm menu:
+            Config.AddSubMenu(new Menu("JungleFarm", "JungleFarm"));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseQJFarm", "Use Q").SetValue(true));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseWJFarm", "Use W").SetValue(true));
+            Config.SubMenu("JungleFarm").AddItem(new MenuItem("UseEJFarm", "Use E").SetValue(true));
+            Config.SubMenu("JungleFarm")
+                .AddItem(
+                    new MenuItem("JungleFarmActive", "JungleFarm!").SetValue(
+                        new KeyBind(Config.Item("LaneClear").GetValue<KeyBind>().Key, KeyBindType.Press)));
+
+            //Misc
+            Config.AddSubMenu(new Menu("Misc", "Misc"));
+            Config.SubMenu("Misc").AddItem(new MenuItem("InterruptSpells", "Interrupt spells").SetValue(true));
+            Config.SubMenu("Misc")
+                .AddItem(
+                    new MenuItem("CastQE", "QE closest to cursor").SetValue(new KeyBind("T".ToCharArray()[0],
+                        KeyBindType.Press)));
+            Config.SubMenu("Misc").AddSubMenu(new Menu("Dont use R on", "DontUlt"));
+
+            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(enemy => enemy.Team != Player.Team))
+                Config.SubMenu("Misc")
+                    .SubMenu("DontUlt")
+                    .AddItem(new MenuItem("DontUlt" + enemy.BaseSkinName, enemy.BaseSkinName).SetValue(false));
+
         }
     }
 }
